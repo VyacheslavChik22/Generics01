9d5840d1c577f6c26076fbbbcf89039899eb829f
VyacheslavChik22 <asyofka@mail.ru>
2024-09-16 22:51:32 +0300

    String generic

--- src/Generics.java
+++ src/Generics.java
@@ -1,10 +1,16 @@
 public class Generics {
     public static void main(String[] args) {
-        Example<Integer> example = new Example<>();
-        example.addToString(1);
-        example.addToString(2);
-        example.addToString(3);
-        example.addToString(4);
+//        Example<Integer> example = new Example<>();
+//        example.addToString(1);
+//        example.addToString(2);
+//        example.addToString(3);
+//        example.addToString(4);
+
+        Example<String> example = new Example<>();
+        example.addToString("Один");
+        example.addToString("Два");
+        example.addToString("Три");
+        example.addToString("Четыре");
 
         System.out.println(example.getStr());
 
